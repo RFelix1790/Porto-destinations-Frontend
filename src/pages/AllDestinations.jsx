aa64e5ecda978d665b6ff846581a548e7159dacd
RFelix1790 <rmsfelix19@gmail.com>
2025-07-24 22:10:39 +0200

    i can see thingsss

--- src/pages/AllDestinations.jsx
+++ src/pages/AllDestinations.jsx
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function AllDestinations() {
+  const [destinations, setdestinations] = useState([]);
+  useEffect(() => {
+    async function getAllDestinations() {
+      try {
+        const response = await axios.get("http://localhost:5005/destinations");
+        setdestinations(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAllDestinations();
+  }, []);
+
   return (
     <div>
-      <h1>all destinations</h1>
+      <h1>All destinations</h1>
+      {destinations.map((eachDestination) => {
+        return (
+          <div className="card" key={eachDestination.id}>
+            <h1>{eachDestination.name}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
