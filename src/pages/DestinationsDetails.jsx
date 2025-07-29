9dcb753e3bc80f77c1dbdf17e48845f9d720c176
RFelix1790 <rmsfelix19@gmail.com>
2025-07-29 21:29:21 +0200

    update added

--- src/pages/DestinationsDetails.jsx
+++ src/pages/DestinationsDetails.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function DestinationsDetails() {
   const { destinationId } = useParams();
   const [destination, setDestination] = useState({});
+  const [update, setupdate] = useState();
+  const [updatedDestination, setUpdatedDestination] = useState();
 
   useEffect(() => {
     async function getDestination() {
@@ -13,6 +15,7 @@ function DestinationsDetails() {
           `http://localhost:5005/destinations/${destinationId}`
         );
         setDestination(response.data);
+        setUpdatedDestination(response.data);
       } catch (error) {
         console.log(error);
       }
@@ -20,15 +23,119 @@ function DestinationsDetails() {
     getDestination();
   }, [destinationId]);
 
+  const handleChanges = (e) => {
+    const { name, value, type, checked } = e.target;
+    setUpdatedDestination((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const rep = await axios.put(
+        `http://localhost:5005/destinations/${destinationId}`,
+        updatedDestination
+      );
+      setDestination(rep.data);
+      setupdate(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      {destination ? (
-        <>
-          <h2>{destination.name}</h2> <p>{destination.description}</p>
-        </>
+      {update ? (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input
+              name="name"
+              type="text"
+              value={updatedDestination.name}
+              onChange={handleChanges}
+            />
+          </label>
+
+          <label>
+            Type:
+            <input
+              name="type"
+              type="text"
+              value={updatedDestination.type}
+              onChange={handleChanges}
+            />
+          </label>
+
+          <label>
+            Description:
+            <input
+              name="description"
+              type="text"
+              value={updatedDestination.description}
+              onChange={handleChanges}
+            />
+          </label>
+
+          <label>
+            Location:
+            <input
+              name="location"
+              type="text"
+              value={updatedDestination.location}
+              onChange={handleChanges}
+            />
+          </label>
+
+          <label>
+            Open on weekends
+            <input
+              name="openOnWeekends"
+              type="checkbox"
+              checked={updatedDestination.openOnWeekends}
+              onChange={handleChanges}
+            />
+          </label>
+
+          <label>
+            Speciality:
+            <input
+              name="speciality"
+              type="text"
+              value={updatedDestination.speciality}
+              onChange={handleChanges}
+            />
+          </label>
+          <label>
+            Rating:
+            <input
+              name="rating"
+              type="number"
+              value={updatedDestination.rating}
+              min="0"
+              max="5"
+              step="1"
+              onChange={handleChanges}
+            />
+          </label>
+          <button type="submit">Confirm</button>
+        </form>
       ) : (
-        <p>Loanding</p>
+        <div>
+          {destination ? (
+            <>
+              <h2>{destination.name}</h2> <p>{destination.description}</p>
+            </>
+          ) : (
+            <p>Loanding</p>
+          )}
+          <Link to="/all-Destinations">Back</Link>
+        </div>
       )}
+      <button onClick={() => setupdate(!update)}>
+        {update ? "cancel" : "Update"}
+      </button>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,17 @@
 function HomePage() {
-  return <div></div>;
+  return (
+    <div>
+      <section>
+        <h1 className="main-Header">Discover the Undefeated City</h1>
+        <img
+          src=""
+          alt="View of Ribeira"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
+        <p>Search Destination</p>
+      </section>
+    </div>
+  );
 }
 
 export default HomePage;
